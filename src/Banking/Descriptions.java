e0be073804b5452662e88402a9f9d53981f7005f
Ma-ri-em <offisss@i.ua>
2016-08-01 17:19:24 +0300

    Update Descriptions.java

--- src/Banking/Descriptions.java
+++ src/Banking/Descriptions.java
@@ -1,7 +1,43 @@
-package Banking;
+package InternetBanking;
 
 /**
- * Created by Don on 31.07.2016.
+ * Created by User on 01.08.2016.
  */
 public class Descriptions {
+
+    private String name;
+
+    public Descriptions(PaymentType type) {
+        name = getPaymentTipe(type);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(PaymentType type) {
+        this.name = getPaymentTipe(type);
+    }
+
+    public String getPaymentTipe(PaymentType type) {
+        String result = "";
+        switch (type) {
+            case paymentForServices:
+                result = "paymentForServices";
+
+            case TransferPersonally:
+                result = "TransferPersonally";
+
+            case transferToAnotherPerson:
+                result = "transferToAnotherPerson";
+
+            case userPaying:
+                result = "userPaying";
+
+            default:
+
+                break;
+        }
+        return result;
+    }
 }
